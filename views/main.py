5f1125c5ac136dc2820ce6648869fd1f9559d4c4
reptile6 <blora616@gmail.com>
2020-06-19 21:36:35 -0400

    typing

--- views/main.py
+++ views/main.py
@@ -1,5 +1,7 @@
+from typing import Tuple, Sequence
+
 from flask import Blueprint, request, jsonify
-from models import Appointment, Trade, User, user_trade
+from models import Appointment, Trade, User, user_trade, Address
 from utils import map_json_to_appointment, map_json_to_address
 from flask_jwt_extended import get_jwt_claims
 from app import db, admin_required, auth_required
@@ -9,14 +11,14 @@ main = Blueprint('main', __name__)
 
 @main.route('/appointments', methods=['GET'])
 @admin_required
-def get_all_appointments():
+def get_all_appointments() -> Tuple[Sequence[Appointment], int]:
     appointments = Appointment.query.all()
     return jsonify(appointments=[a.serialize() for a in appointments]), 200
 
 
 @main.route('/appointments', methods=['POST'])
 @auth_required
-def create_appointment():
+def create_appointment() -> Tuple[Appointment, int]:
     appointment = map_json_to_appointment(request.json)
     provider_id = get_jwt_claims().get('userId', None)
     provider = User.query.filter_by(id=provider_id).first()
@@ -31,7 +33,7 @@ def create_appointment():
 
 @main.route('/addresses', methods=['POST'])
 @auth_required
-def create_address():
+def create_address() -> Tuple[Address, int]:
     address = map_json_to_address(request.json)
     user = User.query.filter_by(id=address.user_id).first()
 
@@ -44,14 +46,14 @@ def create_address():
 
 @main.route("/addresses/<id>", methods=['GET'])
 @auth_required
-def get_addresses_by_user(id):
+def get_addresses_by_user(id) -> Tuple[Sequence[Address], int]:
     user = User.query.filter_by(id=id).first()
     return jsonify(addresses=[a.serialize() for a in user.addresses]), 200
 
 
 @main.route("/appointments/<id>", methods=['GET'])
 @auth_required
-def get_appointments_by_user(id):
+def get_appointments_by_user(id) -> Tuple[Sequence[Appointment], int]:
     user = User.query.filter_by(id=id).first()
     appointments = []
     if user.user_type == 'CLIENT':
@@ -64,7 +66,7 @@ def get_appointments_by_user(id):
 
 @main.route('/appointments/appointment-type', methods=['GET'])
 @auth_required
-def get_appointments_by_type():
+def get_appointments_by_type() -> Tuple[Sequence[Appointment], int]:
     user_id, type = request.args['id'], request.args['type']
     user = User.query.filter_by(id=user_id).first()
     appointments = []
@@ -79,14 +81,14 @@ def get_appointments_by_type():
 
 @main.route('/trades', methods=['GET'])
 @auth_required
-def get_all_trades():
+def get_all_trades() -> Tuple[Sequence[Trade], int]:
     trades = Trade.query.all()
     return jsonify(trades=[t.serialize() for t in trades]), 200
 
 
 @main.route('/trade/<trade>', methods=['GET'])
 @auth_required
-def get_providers_by_trade(trade):
+def get_providers_by_trade(trade) -> Tuple[Sequence[User], int]:
     providers = User.query.join(user_trade).join(Trade).filter(
         (user_trade.c.user_id == User.id) & (user_trade.c.trade_id == Trade.id)).filter_by(name=trade).all()
 
